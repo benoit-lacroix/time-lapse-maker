8ae3df2af7bb62b31d8583392fdf357ab902bb2b
bl5710 <benoit.lacroix@outlook.com>
2021-01-04 18:11:07 +0100

    Migration to Apache Pivot
    Migration whole project to Apache Pivot instead of Java FX

---
+++ tlm-common/src/main/java/radix/home/timelapsemaker/TimeLapseMakerApplication.java
@@ -0,0 +1,12 @@
+package radix.home.timelapsemaker;
+
+import org.apache.pivot.wtk.DesktopApplicationContext;
+import radix.home.timelapsemaker.config.TimeLapseMaker;
+
+public class TimeLapseMakerApplication {
+
+    public static void main(String[] args) {
+        DesktopApplicationContext.main(TimeLapseMaker.class, args);
+    }
+
+}
